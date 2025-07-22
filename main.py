5d2b09f29c07b7a15fd4bc34cd0b9de3354caca8
lsm2714 <is3686994@gmail.com>
2025-07-22 13:47:11 +0900

    2번

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-a, b = input().split() 
-print(int(a) / int(b))
+word = input()
+
+word_list = []
+for n in word :
+    word_list.append(n)
+kaka = word_list[-1::-1]
+for i in kaka :
+    print(i, end='')
+
+# or 
+
+jaja = input() 
+print(jaja[::-1])
+
