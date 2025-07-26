0e196d25f39e791065478f51cb9594bc88e48351
lsm2714 <is3686994@gmail.com>
2025-07-26 21:06:08 +0900

    ㅋㅋㅋㅋ

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-def check_pw(pw) : 
-    if 8 <= len(pw) :
-        pass
-    else : 
-        return '올바르지 않은 비밀번호입니다.'
-    if any(c.isdigit() for c in pw) :
-        pass 
-    else : 
-        return '올바르지 않은 비밀번호입니다.'
-    if any(c.isupper() for c in pw) :
-        pass 
-    else : 
-        return '올바르지 않은 비밀번호입니다.'
-    if any(c.islower() for c in pw) :
-        pass 
-    else : 
-        return '올바르지 않은 비밀번호입니다.'
-    if any(c in '!@#$%^&*' for c in pw) :
-        pass 
-    else : 
-        return '올바르지 않은 비밀번호입니다.'
-    return '올바른 비밀번호입니다.'
+value1 = int(input('첫 번째 수 : '))
+value2 = int(input('두 번째 수 : '))
+value3 = int(input('세 번째 수 : '))
 
-pw = input() 
-
-result = check_pw(pw)
-
-print(result)
+if value1 == value2 == value3 :
+    print('모든 수가 같습니다.')
+    
+if (value1 == value2 and value1 != value3) :
+    print(f'두 수가 같습니다. {value1}와 {value2}')
+if (value1 == value3 and value2 != value3) :
+    print(f'두 수가 같습니다. {value1}와 {value3}')
+if (value3 == value2 and value1 != value2) :
+    print(f'두 수가 같습니다. {value3}와 {value2}')
+    
+if value1 != value2 and value1 != value3 and value2 != value3 : 
+    list_value = [value2, value1, value3]
+    print(f'모든 수가 다릅니다. 가장 큰 수는 {max(list_value)}입니다.')
