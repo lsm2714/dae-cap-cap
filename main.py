de97381a2b67852515b7998004711931c2ff43c1
lsm2714 <is3686994@gmail.com>
2025-07-26 20:28:44 +0900

    이이이

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-def check_password(PW) :
-    if 8 <= len(PW) :
+def check_pw(pw) : 
+    if 8 <= len(pw) :
         pass
     else : 
-        return '사용 불가: 조건을 만족하지 않습니다.'
-    if any(c.isupper() for c in PW) :
-        pass
-    else : 
-        return '사용 불가: 조건을 만족하지 않습니다.'
-    if any(c.islower() for c in PW) :
-        pass
-    else : 
-        return '사용 불가: 조건을 만족하지 않습니다.'
-    if any(c.isdigit() for c in PW) :
+        return '올바르지 않은 비밀번호입니다.'
+    if any(c.isdigit() for c in pw) :
         pass 
     else : 
-        return '사용 불가: 조건을 만족하지 않습니다.'
-    if any(c in "!@#$%^&*" for c in PW):
-        return '사용 가능한 비밀번호입니다.'
-    else:
-        return '사용 불가: 조건을 만족하지 않습니다.'
+        return '올바르지 않은 비밀번호입니다.'
+    if any(c.isupper() for c in pw) :
+        pass 
+    else : 
+        return '올바르지 않은 비밀번호입니다.'
+    if any(c.islower() for c in pw) :
+        pass 
+    else : 
+        return '올바르지 않은 비밀번호입니다.'
+    if any(c in '!@#$%^&*' for c in pw) :
+        pass 
+    else : 
+        return '올바르지 않은 비밀번호입니다.'
+    return '올바른 비밀번호입니다.'
 
+pw = input() 
 
-PW = input() 
-
-result = check_password(PW)
+result = check_pw(pw)
 
 print(result)
