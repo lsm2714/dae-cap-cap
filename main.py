42a0ce098fa562f68f8d54ef8b67ce381425d111
lsm2714 <is3686994@gmail.com>
2025-07-27 20:32:43 +0900

    대 캡 캡

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-number = int(input('현재 온도(섭씨)를 입력하세요 : '))
+num1 = float(input('기본값을 입력하세요 : '))
 
-if number >= 30 :
-    result = '수영'
-elif number >= 20 :
-    result = '등산'
-elif number >= 10 :
-    result = '자전거 타기'
-else : 
-    result = '스키'
+print('''1. 더하기
+2. 빼기
+3. 곱하기
+4. 나누기''')
 
-print(f'\n현재 온도 : {float(number)}도')
-print(f'추천 활동 : {result}')
+def select(num1) : 
+    sel = int(input('선택: '))
+    if sel == 1 :
+        num2 = int(input('숫자 입력: '))
+        return f'연산 결과: {num1 + num2}'
+    elif sel == 2 :
+        num2 = int(input('숫자 입력: '))
+        return f'연산 결과: {num1 - num2}'
+    elif sel == 3 :
+        num2 = int(input('숫자 입력: '))
+        return f'연산 결과: {num1 * num2}'
+    elif sel == 4 :
+        num2 = int(input('숫자 입력: '))
+        if num2 == 0 :
+            return '에러 : 0으로 나눌 수 없습니다.'
+        else : 
+            return f'연산 결과: {num1 / num2}'
+    else : 
+        return '에러 : 범위 밖의 숫자를 선택하셨습니다.'
+
+result = select(num1) 
+
+print(result)
+
+
+
+
     
