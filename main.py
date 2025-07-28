6693b10001fa2c38cbd8fb9b644847bb4b6a287f
lsm2714 <is3686994@gmail.com>
2025-07-28 19:20:45 +0900

    대 캡 캡

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-books = []
+food_list = ['피자', '파스타', '샐러드', '스시', '햄버거']
 
-# 반복 설정 
-while True :
-    book = input('도서 제목을 입력하세요 (종료하려면 "종료" 입력): ')
-    if book == '종료' :
-        break
-    books.append(book)
+print(food_list)
+index = int(input('메뉴 인덱스를 입력하세요(0부터 시작): '))
 
-print(f'도서 목록: {books}')
-    
+print(f'선택된 메뉴: {food_list[index]}')
 
 
     
