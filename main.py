55650679e8a9e911fe56146f41ffe3abf0381c49
lsm2714 <is3686994@gmail.com>
2025-07-29 18:13:07 +0900

    이이카

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-food_list = ['피자', '파스타', '샐러드', '스시', '햄버거']
+import random
 
-print(food_list)
-index = int(input('메뉴 인덱스를 입력하세요(0부터 시작): '))
+def password(length) : 
+    upper = 'QAZWSXEDCRFVTGBYHNUJMIKOLP' 
+    lower = upper.lower() 
+    digit = '1234567890'
+    all = upper + lower + digit 
+    
+    while True : 
+        pw_ran = ''
+        for _ in range(length) :
+            pw_ran = pw_ran + random.choice(all)
+        
+        if any(i.isdigit() for i in pw_ran) and any(i.islower() for i in pw_ran) and any(i.isupper() for i in pw_ran) :
+            return pw_ran
 
-print(f'선택된 메뉴: {food_list[index]}')
+pw = int(input('비밀번호의 길이를 입력하세요: '))
+
+if pw <= 3 :
+    print('비밀번호는 4자리 이상이여야 합니다.')
+    exit() 
+
+result = password(pw)
+
+print(result)
 
 
     
