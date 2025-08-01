d02913be8f4cfc0dcdc51aea540c0ffd9c071dba
lsm2714 <is3686994@gmail.com>
2025-08-01 21:50:28 +0900

    딕셔너리 연습

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-# 주민번호 입력 
-while True : 
-    number = input('주민번호를 입력하세요: ')
-    if len(number) != 14 :
-        print('올바른 주민번호 형식이 아닙니다.')
-    else :
-        break
+menu_list = ['입고', '판매', '설정']
+dict_value = {} 
 
-print(f'마스킹된 주민번호: {number[:8]}',end='') 
-print('******')
-    
+# 반복 설정 
+while True : 
+    menu = input('\n작업을 입력하세요 (입고/판매/종료): ')
+    if menu == '종료' :
+        break
+    if menu not in menu_list :
+        print('올바른 입력이 아닙니다.') 
+        continue
+    name = input('상품명을 입력하세요: ')    
+    number = int(input('수량을 입력하세요: '))
+    # 만약 상품명이 dict_value에 없을 경우 새로 추가 
+    if (name not in dict_value) and (menu == '입고'): 
+        dict_value[name] = number
+    # 만약 상품명이 dict_value에 있을 경우 작업에 따라 알맞은 계산 실행 
+    else : 
+        if menu == '입고' : 
+            dict_value[name] += number
+        elif menu == '판매' : 
+            if name not in dict_value :
+                print('등록되지 않은 상품입니다.')
+            else : 
+                dict_value[name] -= number 
+                if dict_value[name] < 1 : 
+                    print('재고 부족')
+                    dict_value[name] += number
+
+# 결과 출력 (재고)
+if dict_value == {} :
+    print('상품이 등록되어 있지 않습니다.')
+else : 
+    print('--- 현재 재고 ---')
+    for k, v in dict_value.items() :
+        print(f'{k}: {v}개')
+
+
+            
+            
     
 
 
