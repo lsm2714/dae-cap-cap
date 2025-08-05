68e7ec275406873537c0cda5c5bd4bc234258147
lsm2714 <is3686994@gmail.com>
2025-08-05 21:18:08 +0900

    단어의 출현 빈도

--- main.py
+++ main.py
@@ -1,53 +1,14 @@
-import random 
+# 공백을 기준으로 문자열에서 특정 단어의 출현 횟수 검출하기 
+input_value = input('문자열 입력: ')
+input_value = input_value.split()
 
-# 3개의 난수 생성 
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-computer = random.sample(numbers, 3)
-print(computer)
-# 반복 설정 
-count = 1 
-while True : 
-    input_value = input(f'시도 {count}: 입력한 숫자 - ')
-    num = input_value.split()
-    num = [int(i) for i in num]
-    # Strike, Ball, Out 판정 설정 
-    st = 0
-    ball = 0
-    out = 0
-    for i in range(3) : 
-        if num[i] == computer[i] :
-            st += 1 
-        elif num[i] in computer : 
-            ball += 1
-        else : 
-            out += 1
-    print(f'결과: {st} Strike, {ball} Ball', end='')
-    if out >= 1 :
-        print(f', {out} Out')
-    else : 
-        print()
-    
-    count += 1
-    
-    # 승리, 패배 조건 설정 
-    if st == 3 :
-        print(f'게임 종료: 승리\n정답: ', end='')
-        for i in computer :
-            print(i, end=' ')
-        print()
-        break
-    elif out == 3 : 
-        print(f'게임 종료: 패배\n정답: ', end='')
-        for i in computer :
-            print(i, end=' ')
-        print()
-        break
-    if count == 6 :
-        print(f'게임 종료: 패배 (시도 횟수 5회 초과)\n정답: ', end='')
-        for i in computer :
-            print(i, end=' ')
-        print()
-        break
+word = input('단어 입력: ')
+
+count = 0 
+for i in input_value :
+    if i == word :
+        count += 1
+print(f'단어 "{word}"의 출현 빈도: {count}')
     
 
 
