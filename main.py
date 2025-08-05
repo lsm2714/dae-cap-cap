ffb918d619e3a148217c0d3501320e4490994265
lsm2714 <is3686994@gmail.com>
2025-08-05 21:11:10 +0900

    야구겜

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
 import random 
 
-# 랜덤 숫자 선택 
-random_num = random.choice(range(1, 101))
-
+# 3개의 난수 생성 
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+computer = random.sample(numbers, 3)
+print(computer)
 # 반복 설정 
-count = 1
+count = 1 
 while True : 
-    input_value = int(input('숫자를 맞춰보세요 (1~100): '))
-    if input_value > random_num :
-        print('DOWN!')
-        count += 1
-    elif input_value < random_num : 
-        print('UP!')
-        count += 1
+    input_value = input(f'시도 {count}: 입력한 숫자 - ')
+    num = input_value.split()
+    num = [int(i) for i in num]
+    # Strike, Ball, Out 판정 설정 
+    st = 0
+    ball = 0
+    out = 0
+    for i in range(3) : 
+        if num[i] == computer[i] :
+            st += 1 
+        elif num[i] in computer : 
+            ball += 1
+        else : 
+            out += 1
+    print(f'결과: {st} Strike, {ball} Ball', end='')
+    if out >= 1 :
+        print(f', {out} Out')
     else : 
-        print(f'정답입니다! 총 {count}회 만에 맞추셨습니다.')
+        print()
+    
+    count += 1
+    
+    # 승리, 패배 조건 설정 
+    if st == 3 :
+        print(f'게임 종료: 승리\n정답: ', end='')
+        for i in computer :
+            print(i, end=' ')
+        print()
+        break
+    elif out == 3 : 
+        print(f'게임 종료: 패배\n정답: ', end='')
+        for i in computer :
+            print(i, end=' ')
+        print()
+        break
+    if count == 6 :
+        print(f'게임 종료: 패배 (시도 횟수 5회 초과)\n정답: ', end='')
+        for i in computer :
+            print(i, end=' ')
+        print()
         break
     
+
 
             
             
