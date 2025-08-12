e92ee6dbafc7104c93faaea7db31c4a3d88a351c
lsm2714 <is3686994@gmail.com>
2025-08-12 14:39:42 +0900

    딕셔너리와 for 반복문 활용 연습

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 # 학생들의 점수 리스트 
-scores = [92, 85, 34, 76, 58, 90, 61, 70, 45, 99]
+scores = [92, 85, 34, 76, 58, 90, 61, 70, 45, 99, 82, 67, 50, 77, 89]
 
 # 등급별로 딕셔너리에 저장 
 scores_dict = { 
-    '우수': [i for i in scores if i >= 90],
-    '양호': [i for i in scores if 90 > i >= 70],
-    '보통': [i for i in scores if 70 > i >= 50],
-    '미흡': [i for i in scores if 50 > i]
+    'A': [i for i in scores if i >= 90],
+    'B': [i for i in scores if 90 > i >= 80],
+    'C': [i for i in scores if 80 > i >= 70],
+    'D': [i for i in scores if 70 > i >= 60],
+    'F': [i for i in scores if 60 > i]
 }
-    
-# 각 등급에 들어있는 점수와 평균 출력 
+
+# 각 등급의 평균 점수와 그래프 출력 
+print('등급 분포 및 평균 점수: ')
 for k, v in scores_dict.items() : 
-    print(f'{k}: {v} 평균: {(sum(v) / len(v)):.2f}')
+    print(f'{k}등급: 평균 점수 = {(sum(v) / len(v)):.2f}')
+    for _ in v : 
+        print('*', end='')
+    print(f' ({len(v)}명)')
     
             
     
