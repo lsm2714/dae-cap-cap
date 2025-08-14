f132f11210a68be77b045b40e86eb7387559e727
lsm2714 <is3686994@gmail.com>
2025-08-14 18:40:43 +0900

    가장 긴 문자열 찾기

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-# 이메일 유효성 검사 
-email = input() 
+# 가장 긴 단어 찾기 
+input_value = input() 
+list_value = input_value.split() 
 
-if '@' and '.' in email : 
-    print('Valid')
-else : 
-    print('Invalid')
+kaka = [] 
+for i in list_value : 
+    kaka.append(i)
+    
+for _ in kaka : 
+    if len(kaka[0]) > len(kaka[1]) : 
+        del kaka[1]
+    else : 
+        del kaka[0]
+print(kaka[0])
 
 
 
