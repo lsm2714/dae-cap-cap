dba04bde78ac5e9859c2825c31520c0ac8508b56
lsm2714 <is3686994@gmail.com>
2025-08-15 11:29:02 +0900

    연습

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-# 리스트 설정  
+# 문장 입력 받기 
+input_value = input('문장을 입력하세요: ') 
+words = input_value.split() 
+
+# 4글자 이상인 문자만 리스트에 담기 
 list_value = []
+for w in words : 
+    if len(w) >= 4 :
+        list_value.append(w.upper())
 
-# 학생의 이름, 나이, 점수 입력 
-while True : 
-    student = input()
-    if student == 'END' or student == 'end' :
-        break
-    lv = student.split() 
-    lv[1] = int(lv[1])
-    lv[2] = int(lv[2])
-    list_value.append(lv)  
+# 공백으로 나눠서 출력하기 
+print(' '.join(list_value))
 
-# 20세 이상, 80점 이상의 2가지 조건을 모두 충족하는 학생 출력 
-if list_value == [] : 
-    print('저장된 학생 정보가 없습니다.')
-else : 
-    print('--- 20대 이상 80점 이상인 학생 정보 ---')
-    for lis in list_value : 
-        if lis[1] >= 20 and lis[2] >= 80 :
-            for i in lis : 
-                print(i, end=' ')
-            print() 
+
             
         
         
