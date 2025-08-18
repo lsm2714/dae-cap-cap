cbc9a821b13c1106691e0e6dd92d6d5710de4408
lsm2714 <is3686994@gmail.com>
2025-08-18 13:08:55 +0900

    연습

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-# 문자열 입력 
+# 문자열 입력, 리스트에 저장 
 input_value = input()
-
-# 띄어쓰기를 기준으로 리스트에 담기 
 list_value = input_value.split() 
 
-# 글자 수가 짝수인 단어만 출력하기 
-for i in list_value : 
-    if len(i) % 2 == 0 :
-        print(i)
-        
+# 딕셔너리에 각 단어의 길이와 함께 저장 
+dict_value = {} 
+for element in list_value : 
+    if element not in dict_value : 
+        dict_value[element] = len(element)
+
+# 딕셔너리 출력 
+print(dict_value)
+
+# value가 5 이상인 key값만 출력 
+keys = [k for k, v in dict_value.items() if v >= 5]
+if keys != [] : 
+    print('value가 5 이상인 key값들: ', ', '.join(keys))
+else : 
+    print('value가 5 이상인 key값들: 없음')
+    
     
 
 
