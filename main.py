098d6685f95866aab6d0c9b5cd77f217ebbcafe5
lsm2714 <is3686994@gmail.com>
2025-08-18 12:46:07 +0900

    1번

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-# 문자 입력 받기 
-input_value = input('문자열을 입력하세요: ') 
-words = input_value.split() 
+# 문자열 입력 
+input_value = input()
 
-bad_word = input('금지 문자를 입력하세요: ')
+# 띄어쓰기를 기준으로 리스트에 담기 
+list_value = input_value.split() 
 
-# 금지어 포함된 문자 구분 
-list_value = [] 
-for w in words : 
-    if bad_word in w : 
-        continue
-    list_value.append(w)
-
-# 결과 출력 
-print(' '.join(list_value))
-            
-        
+# 글자 수가 짝수인 단어만 출력하기 
+for i in list_value : 
+    if len(i) % 2 == 0 :
+        print(i)
         
     
 
