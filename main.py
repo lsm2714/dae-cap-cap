a56829b964eaa0e179415172eb60a5fa4963e7dd
lsm2714 <is3686994@gmail.com>
2025-08-19 12:48:18 +0900

    연습

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-# 문자열을 숫자와 함께 입력받기 
-input_value = input()
+# 문자열 입력받기 
+input_value = input('문장을 입력하세요: ')
+words = input_value.split()
 
-add = 0 
-numbers = [str(i) for i in range(10)]
-for num in input_value : 
-    if num in numbers : 
-        add += int(num)
+# 문자열 길이에 따라 딕셔너리에 저장하기 
+dict_value = {} 
+for w in words : 
+    if len(w) not in dict_value : 
+        dict_value[len(w)] = [w]
+    else : 
+        dict_value[len(w)].append(w)
 
-print(f'총 합계: {add}')
-
-
-    
-
-
-
+# 결과 출력하기 
+if dict_value == {} :
+    print('입력한 문자가 없습니다.')
+else : 
+    print('--- 입력한 문자들의 길이 ---')
+    for k, v in dict_value.items() :
+        print(f'{k}: {', '.join(v)}')
 
 
 
