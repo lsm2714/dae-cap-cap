1b2e5a03ea0c34570f6da243879dbc461b46b3c4
lsm2714 <is3686994@gmail.com>
2025-08-20 11:01:46 +0900

    소수 판별기

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-# 문자열 입력받기 
-input_value = input('문장을 입력하세요: ')
-words = input_value.split()
-words = [i.lower() for i in words]
+# 소수 판별기 
+# 숫자 입력 후 리스트에 담기 
+number = int(input('숫자를 입력하세요: ')) 
+numbers = [num for num in range(2, number + 1)]
 
-# 단어가 나온 횟수를 딕셔너리에 저장 
-dict_value = {} 
-for w in words : 
-    if w not in dict_value : 
-        dict_value[w] = 1 
-    else : 
-        dict_value[w] += 1
+# for 반복문으로 소수 판별하기 
+list_value = []
+for num1 in numbers : 
+    check_list = []
+    for num2 in range(2, num1) : 
+        num = num1 % num2
+        if num == 0 : 
+            check_list.append(num)
+    if check_list == [] :
+        list_value.append(num1)
 
-# 딕셔너리 출력, 최빈값 출력 
-if dict_value == {} :
-    print('입력된 단어가 없습니다.')
-else : 
-    values = []
-    max_keys = []
-    print('--- 단어 빈도 ---')
-    for k, v in dict_value.items() :
-        print(f'{k}: {v}회')
-    max_value = max([v for v in dict_value.values()])
-    max_keys = [k for k in dict_value.keys() if dict_value[k] == max_value]
-    print(f'가장 많이 나온 단어: {max_keys} ({max_value}회)')
+print(list_value)
+
+
+        
+        
 
 
 
