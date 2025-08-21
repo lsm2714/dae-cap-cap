dc5153bfa082c4141bca6fc02b3e87295f435255
lsm2714 <is3686994@gmail.com>
2025-08-21 10:39:30 +0900

    연습

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-# 중복 제거 후 정렬
-# 숫자 입력 후 리스트에 담기 
-numbers = input('숫자들을 입력하세요: ')
-numbers = [int(i) for i in numbers if i != ' ']
-
-# 중복 제거 후 리스트에 오름차 순으로 담기 
-list_value = [] 
-for num in numbers : 
-    if num not in list_value : 
-        list_value.append(num)
-
-num_list = [] 
-while list_value != [] : 
-    min_value = min(list_value)
-    num_list.append(min_value)
-    for i, v in enumerate(list_value) :
-        if v == min_value : 
-            del list_value[i]
-    
-print(num_list)
-    
+# 모음 세기 
+# 문자열 입력받기 
+input_value = input() 
+aiueo = ['a', 'i', 'u', 'e', 'o']
+list_value = [i for i in input_value if i in aiueo]
+print(len(list_value))
     
     
 
