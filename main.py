a6a38ef3a9d994059c0a9eddbe24eb36f326795d
lsm2714 <is3686994@gmail.com>
2025-08-21 11:56:58 +0900

    연습

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-# 학생 성적 출력, 최고점 학생(들) 출력 
-# 종료 입력할 때까지 반복 설정 
-dict_value = {} 
-print('학생 이름과 성적을 입력하세요 (종료 입력시 종료): ')
-while True : 
-    student = input() 
-    if student == '종료' : 
-        break
-    student = student.split() 
-    if student[0] not in dict_value : 
-        dict_value[student[0]] = [int(student[1])]
+# 문자열 압축 
+# 문자열 입력받기 
+input_value = input('문자열을 입력하세요: ') 
+words = []
+list_value = []
+for i in input_value : 
+    if words and i != words[-1] : 
+        list_value.append(words)
+        words = []
+    words.append(i)
+list_value.append(words)
+
+# 문자열 압축하기 
+count = 0
+str_value = ''
+for i1 in list_value : 
+    if len(i1) == 1 :
+        str_value += i1[0]
     else : 
-        dict_value[student[0]].append(int(student[1]))
+        count = 0
+        for i2 in i1 : 
+            count += 1 
+        str_value += i1[0] + str(count)
 
-# 학생별 평균 점수 출력 
-avgs = 0
-print('\n--- 학생별 평균 점수 ---')
-for k, v in dict_value.items() : 
-    avg = sum(v) / len(v)
-    print(f'{k}: {avg:.1f}')
-    if avg >= avgs :
-        avgs = avg
+if len(input_value) <= len(str_value) : 
+    print(f'압축된 문자: {input_value}')
+else : 
+    print(f'압축된 문자: {str_value}')
 
-names = []
-for k, v in dict_value.items() : 
-    if (sum(v) / len(v)) == avgs : 
-        names.append(k)
-print(f'최고 평균 점수 학생: {names} ({avgs}점)')
+    
 
 
 
