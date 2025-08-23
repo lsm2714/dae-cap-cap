79cd18a6dcd7c4f8a2f721c9c3c4a1d7a4c94408
lsm2714 <is3686994@gmail.com>
2025-08-23 12:20:54 +0900

    연습

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-# 완전수 판별기
+# 피라미드 출력기
 # 정수 입력받기 
 number = int(input('정수를 입력하세요: '))
 
-# 약수 구해서 리스트에 집어넣기 
-list_value = [num1 for num1 in range(1, number + 1) if number % num1 == 0]
-
-# 완전수 구하기 
-if sum(list_value) - number == number : 
-    print(f'{number}는 완전수입니다.')
-else :
-    print(f'{number}는 완전수가 아닙니다.')
-    
-
-
+# 피라미드 출력하기 
+for num in range(1, number + 1) : 
+    for _ in range(number - num) : 
+        print(' ', end=' ')
+    for _ in range(num) : 
+        print('*', end=' ')
+    for _ in range(num - 1) :
+        print('*', end=' ')
+    print()
     
 
         
