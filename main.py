2db3827e95433d0502d2c0bf300bbf98c66fec6f
lsm2714 <is3686994@gmail.com>
2025-08-23 11:45:24 +0900

    완전수 구하기

--- main.py
+++ main.py
@@ -1,34 +1,16 @@
-# 팩토리얼 자릿수 계산 
-# 팩토리얼 입력받기 
-list_value = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!']
-pacto = input('팩토리얼을 입력하세요: ')
-if pacto == '!' :
-    print('팩토리얼을 입력하세요.')
-    exit() 
-for i in pacto : 
-    if i not in list_value or '!' not in pacto : 
-        print('팩토리얼을 입력하세요.')
-        exit() 
+# 완전수 판별기
+# 정수 입력받기 
+number = int(input('정수를 입력하세요: '))
 
-# '!' 제외 후 리스트에 넣기 
-number = pacto.split('!')
-del number[-1]
+# 약수 구해서 리스트에 집어넣기 
+list_value = [num1 for num1 in range(1, number + 1) if number % num1 == 0]
 
-# 팩토리얼 계산하기 
-num = 1
-for n in range(1, int(number[0]) + 1) : 
-    num *= n 
-print(f'팩토리얼 계산 값: {num}')
-
-# 자릿수 계산하기 
-num = str(num)
-num_list = [int(i) for i in num]
-
-num2 = 0 
-for n in num_list : 
-    num2 += n 
-print(f'팩토리얼 자릿수 계산 값: {num2}')
-
+# 완전수 구하기 
+if sum(list_value) - number == number : 
+    print(f'{number}는 완전수입니다.')
+else :
+    print(f'{number}는 완전수가 아닙니다.')
+    
 
 
     
