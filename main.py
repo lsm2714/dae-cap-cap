2be5134e275aea046ab13f83e3bf997a20dbf889
lsm2714 <is3686994@gmail.com>
2025-08-23 11:26:44 +0900

    팩토리얼

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
-# 문자열 압축 
-# 문자열 입력받기 
-input_value = input('문자열을 입력하세요: ') 
-words = []
-list_value = []
-for i in input_value : 
-    if words and i != words[-1] : 
-        list_value.append(words)
-        words = []
-    words.append(i)
-list_value.append(words)
+# 팩토리얼 자릿수 계산 
+# 팩토리얼 입력받기 
+list_value = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!']
+pacto = input('팩토리얼을 입력하세요: ')
+if pacto == '!' :
+    print('팩토리얼을 입력하세요.')
+    exit() 
+for i in pacto : 
+    if i not in list_value or '!' not in pacto : 
+        print('팩토리얼을 입력하세요.')
+        exit() 
 
-# 문자열 압축하기 
-str_value = ''
-for i1 in list_value : 
-    if len(i1) == 1 :
-        str_value += i1[0]
-    else : 
-        str_value += i1[0] + str(len(i1))
+# '!' 제외 후 리스트에 넣기 
+number = pacto.split('!')
+del number[-1]
 
-if len(input_value) <= len(str_value) : 
-    print(f'압축된 문자: {input_value}')
-else : 
-    print(f'압축된 문자: {str_value}')
+# 팩토리얼 계산하기 
+num = 1
+for n in range(1, int(number[0]) + 1) : 
+    num *= n 
+print(f'팩토리얼 계산 값: {num}')
 
-    
+# 자릿수 계산하기 
+num = str(num)
+num_list = [int(i) for i in num]
+
+num2 = 0 
+for n in num_list : 
+    num2 += n 
+print(f'팩토리얼 자릿수 계산 값: {num2}')
 
 
 
