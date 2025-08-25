bec044329b4ea5468b43391c704613570d1ca888
lsm2714 <is3686994@gmail.com>
2025-08-25 11:33:34 +0900

    연습;

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-# 피보나치 수열 
-# 정수형 입력받기 
-number = int(input('입력: '))
-
-pibonachi = [0, 1]
-
-if number < 2 : 
-    if number == 0 : 
-        print(pibonachi[0:1])
-    else :
-        print(pibonachi)
+# 콜라츠 추측 
+# 숫자 입력 받기 
+number = int(input('입력: ')) 
+if number == 1 : 
+    print('총 1단계 만에 1에 도달했습니다.')
 else : 
-    for _ in range(number + 1) :
-        num = pibonachi[-2] + pibonachi[-1]
-        pibonachi.append(num)
-    print(pibonachi[:-2])
-
+    count = 0
+    while number != 1 : 
+        count += 1
+        if number % 2 == 0 : 
+            number /= 2 
+        else :
+            number = number * 3 + 1
+    print(f'총 {count}단계 만에 1에 도달했습니다.') 
         
         
 
