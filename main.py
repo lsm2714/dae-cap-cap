a97d5d69b87e61cee5ed3cf47fdcc8f2c66c5f9b
lsm2714 <is3686994@gmail.com>
2025-08-25 11:32:43 +0900

    연습

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-# 피라미드 출력기
-# 정수 입력받기 
-number = int(input('정수를 입력하세요: '))
+# 피보나치 수열 
+# 정수형 입력받기 
+number = int(input('입력: '))
 
-# 피라미드 출력하기 
-for num in range(1, number + 1) : 
-    for _ in range(number - num) : 
-        print(' ', end=' ')
-    for _ in range(num) : 
-        print('*', end=' ')
-    for _ in range(num - 1) :
-        print('*', end=' ')
-    print()
-    
+pibonachi = [0, 1]
+
+if number < 2 : 
+    if number == 0 : 
+        print(pibonachi[0:1])
+    else :
+        print(pibonachi)
+else : 
+    for _ in range(number + 1) :
+        num = pibonachi[-2] + pibonachi[-1]
+        pibonachi.append(num)
+    print(pibonachi[:-2])
 
         
         
