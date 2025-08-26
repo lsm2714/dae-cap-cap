b6a2255fa98d2006b758e8a0273f69c586198491
lsm2714 <is3686994@gmail.com>
2025-08-26 20:52:56 +0900

    연습

--- main.py
+++ main.py
@@ -1,68 +1,60 @@
-# 도서 관리 프로그램 만들기 
-dict_value = {} 
+# 출석 관리 프로그램 
+dict_value = {}
 
 # 반복 설정 
 while True : 
-    select = int(input(''' ==== 도서 관리 프로그램 ====
-1. 책 추가
-2. 책 삭제
-3. 책 목록 보기 
-4. 책 검색
+    select = int(input('''
+==== 출석 관리 프로그램 ==== 
+1. 학생 추가 
+2. 학생 삭제
+3. 출석 체크
+4. 출석 현황 보기 
 5. 종료
 선택: '''))
     
     # 1번 선택 설정 
-    if select == 1 : 
-        book_name = input('추가할 책 제목: ')
-        lower_name = book_name.lower()
-        if lower_name not in dict_value : 
-            dict_value[lower_name] = 1
+    if select == 1 :
+        student_name = input('학생 이름: ')
+        if student_name not in dict_value :
+            dict_value[student_name] = '결석' 
+            print(f'{student_name} 학생이 추가되었습니다!\n')
         else : 
-            dict_value[lower_name] += 1
-        print(f'추가되었습니다! (현재 권 수: {dict_value[lower_name]}권)\n')
-        
+            print('이미 있는 학생 이름입니다.\n')
+    
     # 2번 선택 설정 
     elif select == 2 : 
-        book_name = input('삭제할 책 제목: ')
-        lower_name = book_name.lower()
-        if lower_name not in dict_value : 
-            print('저장되지 않은 책입니다.\n')
+        student_name = input('삭제할 학생 이름: ')
+        if student_name in dict_value : 
+            del dict_value[student_name] 
+            print('학생 정보가 삭제되었습니다.\n')
         else : 
-            dict_value[lower_name] -= 1
-            if dict_value[lower_name] == 0 :
-                del dict_value[lower_name]
-                print('해당 책의 마지막 권을 삭제했습니다. 목록에서 제거됩니다.\n')
-            else : 
-                print(f'1권 삭제되었습니다. (남은 권 수: {dict_value[lower_name]}권)\n')
+            print('해당 학생의 이름이 저장되어 있지 않습니다.\n')
     
     # 3번 선택 설정 
-    elif select == 3 :
-        print('현재 책 목록: ')
-        if dict_value == {} : 
-            print('저장되어 있는 책이 없습니다.\n')
+    elif select == 3 : 
+        check = input('출석 체크할 학생 이름: ')
+        if check in dict_value : 
+            dict_value[check] = '출석'
+            print(f'{check} 학생이 출석 처리되었습니다.\n')
         else : 
-            for k, v in dict_value.items() :
-                print(f'- {k} {v}권')
-            print() 
-        
+            print('해당 학생의 이름이 저장되어 있지 않습니다.\n')
+    
     # 4번 선택 설정 
     elif select == 4 : 
-        count = 0
-        searching = input('검색 키워드: ')
-        print('검색 결과: ')
-        for k, v in dict_value.items() :
-            if searching.lower() in k : 
-                print(f'- {k} {v}권')
-                count += 1
-        if count == 0 :
-            print('검색 결과가 없습니다.')
-        print()
-    
-    # 잘못된 입력 설정 
-    elif select > 5 or select < 1 :
-        print('잘못된 입력입니다.\n')
-    
-    # 5번 종료 설정 
-    else : 
+        print('출석 현황: ')
+        if dict_value != {} :
+            for k, v in dict_value.items() : 
+                print(f'- {k}: {v}')
+            print()
+        else : 
+            print('저장된 학생 정보가 없습니다.\n')
+
+    # 5번 선택 설정 
+    elif select == 5 :
         break
+
+    # 잘못된 입력 설정 
+    else :
+        print('잘못된 입력입니다. 1부터 5 사이의 숫자 중에서 선택해 주세요.\n')
+
 print('프로그램을 종료합니다.')
