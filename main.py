1556d091b122b71543979268cd2df6f9f1ea48e4
lsm2714 <is3686994@gmail.com>
2025-08-27 11:53:24 +0900

    연습

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-# 구구단 출력기
-list_value = [i for i in range(2, 10)]
+# 숫자 빈도 출력기 
+dict_value = {} 
 
-# 단 입력받기 
-gugudan = int(input('단을 입력하세요 (2단부터 9단까지): '))
-
-if gugudan in list_value : 
-    for i in list_value : 
-        print(f'{gugudan} X {i} = {gugudan * i}')
-else : 
-    print('잘못된 입력입니다. 2단부터 9단까지 입력해 주세요.')
+# 반복 설정 
+while True : 
     
+    # 정수 입력
+    number = int(input('정수 입력 (0 입력 시 종료): '))
+    
+    # 입력이 0이 아닐 경우 설정 
+    if number != 0 :
+        # dict_value에 없을 경우 새로 생성 후 value값을 1로 설정 
+        if number not in dict_value : 
+            dict_value[number] = 1
+        # 있을 경우 value값에 1 추가 
+        else : 
+            dict_value[number] += 1 
+    # 0일 경우 break로 반복문 빠져나가기
+    else : 
+        break
+
+# 결과 출력 
+print('결과: ')
+if dict_value == {} : 
+    print('저장된 숫자가 없습니다.')
+else : 
+    for k, v in (sorted(dict_value.items())) : 
+        print(f'{k}: {v}회')
 
