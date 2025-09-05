558e162d05806ab7d46659fd91a9f49de41f06d5
lsm2714 <is3686994@gmail.com>
2025-09-05 15:56:35 +0900

    연습

--- main.py
+++ main.py
@@ -1,67 +1,61 @@
-# 학생 성적 관리 프로그램 
-dict_value = {} 
-scores = []
+# 단어 퀴즈 게임 만들기
+import random
+list_words = []
+correct = 0
+wrong = 0 
+score = 0
 
-# 학생 정보 입력 설정 (반복)
-print('학생 이름과 점수를 입력하세요 (종료 입력 시 멈춤): ')
-while True : 
-    student = input()
-    if student == '종료' : 
-        break
-    student = student.split() 
-    dict_value[student[0]] = int(student[1])
+# 딕셔너리에 퀴즈 단어 저장 + 단어만 따로 저장 
+dict_value = {
+    'おはようございます' : '안녕하세요',
+    '権利' : '권리',
+    '謙遜' : '겸손',
+    '金融' : '금융',
+    '霜' : '서리',
+    '霧' : '안개',
+    '相性' : '궁합이 맞음',
+    'カラス' : '까마귀',
+    'ひぐらし' : '쓰르라미'
+}
 
-# 바로 종료 입력 시 
-if dict_value == {} : 
-    print('\n저장된 학생 정보가 없습니다.')
-    exit()
-    
-# 메뉴 선택 설정 (반복)
+for k in dict_value.keys() :
+    list_words.append(k)
+
+# 풀 문제의 개수 입력 
 while True : 
-    select = int(input('''\n메뉴를 선택하세요: 
-1. 전체 학생 목록 보기 
-2. 평균 점수 구하기
-3. 최고 점수 학생 찾기
-4. 특정 학생 점수 조회
-5. 종료
-선택: '''))
-    
-    # scores 리스트에 점수 집어넣기 
-    for v in dict_value.values() :
-        scores.append(v)
-        
-    # 1번 선택 설정 
-    if select == 1 : 
-        for k, v in dict_value.items() :
-            print(f'- {k}: {v}점')
-            
-    # 2번 선택 설정 
-    elif select == 2 :
-        print(f'평균 점수: {(sum(scores) / len(scores)):.1f}')
-    
-    # 3번 선택 설정 
-    elif select == 3 :
-        max_score = max(scores)
-        for k in dict_value.keys() : 
-            if dict_value[k] == max_score :
-                print(f'최고 점수 학생: {k} ({max_score}점)')
-    
-    # 4번 선택 설정 
-    elif select == 4 :
-        search = input('조회할 학생 이름: ')
-        if search not in dict_value : 
-            print('저장되어 있지 않은 학생입니다.')
-        else : 
-            print(f'{search}의 점수: {dict_value[search]}점')
-        
-    # 5번 선택 설정 
-    elif select == 5 : 
-        break
-    
-    # 잘못된 입력 설정 
+    number = int(input('몇 문제를 푸시겠습니까? '))
+    if number > len(dict_value) or number < 1 : 
+        print('범위를 벗어났습니다.')
+        continue
     else : 
-        print('잘못된 입력입니다. 1부터 5 사이의 숫자 중 선택하세요.')
-        
-print('프로그램을 종료합니다.')
-            
-    
+        break
+
+# 퀴즈 단어 출력 + 퀴즈 풀이 
+count = 1
+for i in random.sample(list_words, number) : 
+    quiz = input(f'문제 {count}: {i}의 뜻은? -> ')
+    # 정답일 경우
+    if quiz == dict_value[i] :
+        print('정답입니다!\n')
+        correct += 1
+        score += 10
+    # 오답일 경우 
+    else : 
+        print(f'오답입니다. 정답: {dict_value[i]}\n')
+        wrong += 1
+    count += 1
+
+# result 설정 
+if correct / 2 < wrong : 
+    result = '더 노력하세요!'
+elif correct / 2 > wrong :
+    result = '훌륭합니다!'
+else : 
+    result = '잘했어요!'
+
+# 결과 출력 
+print('=== 결과 ===')
+print(f'정답 개수: {correct}')
+print(f'오답 개수: {wrong}')
+print(f'점수: {score}')
+print(f'{result}')
