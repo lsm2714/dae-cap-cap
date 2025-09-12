cf8d4967600f9a596e477fa882e4e1e023ab43ec
lsm2714 <is3686994@gmail.com>
2025-09-12 23:29:55 +0900

    연습

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
-# 도서 대출 관리 프로그램 
-dict_value = {}
+# ATM 출금 시뮬레이션 
+dict_value = {} 
+moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+amounts = []
 
-# 반복 설정을 위한 대출 권 수 입력 
+# 돈 입력받기 
+amount = int(input('Enter amount: '))
+
+# 10원 미만 출금 불가 설정 
 while True : 
-    number = int(input('대출할 책의 개수 입력: '))
-    if number < 1 :
-        print('잘못된 입력입니다. 1 이상의 수를 입력해 주세요.')
+    if amount < 10 :
+        print('10원 미만은 출금할 수 없습니다. 다시 입력해 주세요')
+        continue
     else : 
-        break 
+        break
     
-# 책 이름과 권수 반복 입력 
-print('책 이름과 권수 입력 (띄어쓰기 기준): ')
-for _ in range(number) : 
-    book = input().split()
-    # 딕셔너리에 저장 (중복 시 잘못된 입력 처리)
-    if book[0] not in dict_value : 
-        dict_value[book[0]] = int(book[1])
-    # 잘못된 입력 반복 설정
+# 돈 출금 반복 설정 
+for m in moneys : 
+    # 빼는 돈이 바뀌 때마다 count 초기화
+    count = 0 
+    # amount가 m(돈)과 같거나 클 때만 계산 시작 
+    while amount >= m : 
+        amount -= m 
+        count += 1
+    # 딕셔너리에 집어넣기, amouts 리스트에 현재 남은 가격 집어넣기
+    if count != 0 :
+        # 쉼표 붙이기 
+        if m >= 1000 :
+            hit = 0
+            m2 = str(m)
+            m = ''
+            for i in m2[::-1] : 
+                hit += 1
+                m += i
+                if hit == 3 :
+                    m += ','
+            m = m[::-1]
+        dict_value[f'{m}원'] = count
+        amounts.append(amount)
+
+# 결과 출력 
+for k, v in dict_value.items() : 
+    # 장, 개 구분
+    if len(k) > 4 : 
+        print(f'{k}: {v}장', end=' ')
     else : 
-        while True : 
-            print('잘못된 입력입니다. 다시 입력해 주세요.')
-            book = input().split()
-            if book[0] not in dict_value :
-                break
-            else : 
-                continue
-        
-    
+        print(f'{k}: {v}개', end=' ')
+    print(f'(나머지: {amounts.pop(0)}원)')
+
     
