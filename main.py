805e8cfd7ac5ea578943524f39062e1c85a127a1
lsm2714 <is3686994@gmail.com>
2025-09-15 22:39:28 +0900

    연습

--- main.py
+++ main.py
@@ -1,58 +1,30 @@
-# 자판기 시뮬레이션 
-moneys = [500, 100, 50, 10]
-dict_money = {} 
+# 단어 퍼즐 생성기 
+import random
+correct = 0 
+wrong = 0
 
-dict_value = { # 메뉴
-    '콜라' : 1200,
-    '사이다' : 1000,
-    '커피' : 1500,
-    '물' : 700
-} 
+# 띄어쓰기를 기준으로 단어들 입력 
+words = input().split() 
 
-# 투입할 금액 입력 
-money = int(input('투입 금액: '))
-
-# 음료 구매 반복 설정 
-while True : 
-    name = input('구매할 음료: ')
-    # '없음'일 경우 break
-    if name == '없음' :
-        break
-    if name in dict_value : 
-        # 만약 잔액이 충분할 경우 
-        if dict_value[name] <= money :
-            money -= dict_value[name]
-            print(f'{name} 구매 완료')
-        # 잔액 부족일 경우 
-        else : 
-            print('잔액 부족')
-    # 잘못된 입력 설정
+# 문제 랜덤으로 섞기 
+for i in words : 
+    ran_word = ''
+    for r in random.sample(i, len(i)) : 
+        ran_word += r
+    # 문제 출력 
+    print(f'문제: {"".join(ran_word)}')
+    # 정답 입력 설정 
+    input_value = input('정답 입력: ')
+    if input_value == i :
+        print('정답!')
+        correct += 1
     else : 
-        print('잘못된 입력입니다.')
+        print('오답!')
+        wrong += 1
 
-# 잔돈 설정 (반복)
-if money >= 10 :
-    for m in moneys : 
-        count = 0 # 동전 갯수 카운트
-        # 빼기 반복 설정 
-        while True : 
-            if money < m : 
-                break
-            money -= m 
-            count += 1
-        # dict_money에 count가 0 이상인 것만 집어넣기 
-        if count >= 1 :
-            dict_money[f'{m}원'] = f'{count}개'
-    # 잔돈 출력 설정 
-    count = 0 
-    print('잔돈: ', end='')
-    for k, v in dict_money.items() :
-        print(f'{k} {v}', end='') 
-        # 쉼표 설정
-        count += 1
-        if count != len(dict_money) : 
-            print(', ', end='')
-else : 
-    print('\n잔돈 없음')
+# 종료 시 결과 출력 
+print('게임 종료')
+print(f'정답: {correct}개')
+print(f'오답: {wrong}개')
 
     
