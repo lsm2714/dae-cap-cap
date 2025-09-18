a74aae1018e0bbab9e13f10db5826207d8da8443
lsm2714 <is3686994@gmail.com>
2025-09-18 21:14:24 +0900

    함수 복습

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-# 함수 복습 
-def calculate(x, y) :
-    if y == 0 : 
-        q = '0으로 나눌 수 없습니다.'
+# 회문 판별기 (함수 사용)
+def is_Palindrome(w) :
+    if w == w[::-1] : 
+        result = '회문입니다.'
     else : 
-        q = f'{(x/y):.2f}'
-    return x+y, x-y, x*y, q # 이렇게 하면 튜플의 형태로 반환됨 
+        result = '회문이 아닙니다.'
+    return result
 
-# 숫자 2개 입력 
-num1 = int(input('첫 번째 숫자 입력: '))
-num2 = int(input('두 번째 숫자 입력: '))
-a, b, c, d = calculate(num1, num2) # 튜플 자릿수에 맞춰서 변수에 각각 넣어주기
-print(f'합: {a}\n차: {b}\n곱: {c}\n몫: {d}')
+# 단어 입력하기 
+word = input('문자 입력: ').replace(" ", "") # replace = 전 -> 후
+print(is_Palindrome(word.lower()))
